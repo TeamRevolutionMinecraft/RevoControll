ede6dbd4efec686a9ab98653cadd5cc61af3e668
FelixSchmidt <Felix-Schmidt2@outlook.de>
2024-11-25 09:46:27 +0100

    Initial Commit

---
+++ src/main/java/net/revo/revoServer/RevoServer.java
@@ -0,0 +1,17 @@
+package net.revo.revoServer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RevoServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
