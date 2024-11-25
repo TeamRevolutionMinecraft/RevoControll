dd47d20f2294b5ff608ef26fb95e965ca638b750
FelixSchmidt <Felix-Schmidt2@outlook.de>
2024-11-25 14:45:53 +0100

    Initial Command Interface implementation

--- src/main/java/net/revo/revoServer/RevoServer.java
+++ src/main/java/net/revo/revoServer/RevoServer.java
@@ -1,17 +1,37 @@
 package net.revo.revoServer;
 
+import net.revo.revoServer.revoPlayer.RevoPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public final class RevoServer extends JavaPlugin {
+
+    private static RevoServer instance;
+    private HashMap<UUID, RevoPlayer> connectedPlayers;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        connectedPlayers = new HashMap<>();
+        instance = this;
 
+
+        getLogger().info("RevoServer is enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static HashMap<UUID, RevoPlayer> getConnectedPlayers()
+    {
+        return getInstance().connectedPlayers;
+    }
+    public static RevoServer getInstance()
+    {
+        return instance;
     }
 }
---
+++ src/main/java/net/revo/revoServer/cmd/Command.java
@@ -0,0 +1,90 @@
+package net.revo.revoServer.cmd;
+
+import net.revo.revoServer.RevoServer;
+import net.revo.revoServer.revoPlayer.RevoPlayer;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public abstract class Command implements CommandExecutor, TabCompleter {
+    @NotNull
+    protected final RevoServer plugin;
+    @NotNull
+    protected final String commandName;
+    @NotNull
+    protected final List<String> permissions;
+    @NotNull
+    protected final Map<String, Action> actions;
+
+    @FunctionalInterface
+    public interface Action {
+        /**
+         * This method is called when a valid subcommand is provided. Thrown exception are logged.
+         * @param player the player which executes this command
+         * @param args the args of the original command invoke but stripped of the first element
+         */
+        void command(@NotNull RevoPlayer player, @NotNull String[] args);
+    }
+
+    public Command(RevoServer plugin, String commandName, List<String> permissions)
+    {
+        this.plugin = plugin;
+        this.commandName = commandName;
+        this.permissions = permissions == null ? new ArrayList<>() : permissions;
+        Objects.requireNonNull(plugin.getCommand(commandName)).setExecutor(this);
+        this.actions = new HashMap<>();
+
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, org.bukkit.command.@NotNull Command command,
+                             @NotNull String s, @NotNull String[] strings)
+    {
+        if (!this.permissions.isEmpty() && permissions.stream().noneMatch(commandSender::hasPermission))
+        {
+            commandSender.sendMessage("No Permission");
+            return false;
+        }
+        if (commandSender instanceof Player)
+        {
+            Action action;
+            if (strings.length == 0)
+            {
+
+                action = this.actions.get(commandName);
+            } else
+            {
+                action = this.actions.get(strings[0]);
+            }
+
+            try
+            {
+                action.command(RevoPlayer.getOnlinePlayer((Player) commandSender).orElseThrow(),
+                        Arrays.copyOfRange(strings, 1, strings.length));
+                return true;
+            } catch (Exception e) {
+                RevoServer.getInstance().getLogger()
+                        .warning("Error while executing command " + commandSender.getName() + ": " + e.getMessage());
+                return false;
+            }
+
+        }
+        return false;
+    }
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender,
+                                                org.bukkit.command.@NotNull Command command,
+                                                @NotNull String s, @NotNull String[] strings)
+    {
+        return actions.keySet().stream()
+                .filter(key -> !key.equals(commandName))
+                .collect(Collectors.toList());
+    }
+
+}
